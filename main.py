21980ef080a263bfb6e4263c002ea46d8b670c91
Shreyash Hiwale <shreycasm007@gmail.com>
2025-02-08 16:31:35 +0530

    remaining code added

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from data_collection import DataCollection 
+from feature_engineering import FeatureEngineering
 from model_trainer  import ModelTrainer
 
 
+
+
+
+datacollection = DataCollection()
+featureengineering = FeatureEngineering()
 modeltrainer = ModelTrainer()
+
+
+#fetching data from databse and saving locally
+datacollection.save_data()
+
+#cleaning data and saving locally
+featureengineering.save_data()
 
 
 #training model and predicting  
